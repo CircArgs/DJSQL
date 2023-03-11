17a42c475eb4ea8c6fb5d8830dca8fb56166c6b3
Nick Ouellet <nick@ouellet.dev>
2023-03-10 20:58:44 -0500

    instructions update; rebuilt

--- grammar/generated/SqlBaseVisitor.py
+++ grammar/generated/SqlBaseVisitor.py
@@ -1,4 +1,4 @@
-# Generated from SqlBase.g4 by ANTLR 4.12.0
+# Generated from SqlBase.g4 by ANTLR 4.9.3
 from antlr4 import *
 if __name__ is not None and "." in __name__:
     from .SqlBaseParser import SqlBaseParser
